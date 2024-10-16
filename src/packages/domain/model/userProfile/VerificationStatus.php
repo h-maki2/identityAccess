b86c38982e2a4e20c15e7db9cf95c3fc9c230f95
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-16 17:20:27 +0000

    VerificationStatusクラスを追加

---
+++ src/packages/domain/model/userProfile/VerificationStatus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace packages\domain\model\userProfile;
+
+enum VerificationStatus: string
+{
+    case Verified = '1';
+    case Unverified = '0';
+    
+    public function displayValue(): string
+    {
+        return match($this) {
+            self::Verified => '認証済み',
+            self::Unverified => '未認証'
+        };
+    }
+
+    /**
+     * 認証済みかどうかを判定
+     */
+    public function isVerified(): bool
+    {
+        return $this->value === self::Verified;
+    }
+}
