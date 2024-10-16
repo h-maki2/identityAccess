34071d2a7d7031f06706d264ee604f1b1f82d539
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-16 16:50:34 +0000

    UserEmailクラスを追加

---
+++ src/packages/domain/model/userProfile/UserEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace packages\domain\model\userProfile;
+
+use InvalidArgumentException;
+
+class UserEmail
+{
+    private readonly string $value;
+
+    public function __construct(string $value)
+    {
+        if (empty($value)) {
+            throw new InvalidArgumentException('メールアドレスが空です。');
+        }
+
+        if ($this->invalidEmail($value)) {
+            throw new InvalidArgumentException('無効なメールアドレスです。');
+        }
+
+        $this->value = $value;
+    }
+
+    public function localPart(): string
+    {
+        return explode('@', $this->value)[0];
+    }
+
+    private function invalidEmail(string $value): bool
+    {
+        return !preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $value);
+    }
+}
