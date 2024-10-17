5fbf83b40a9528eb829fa114637a757852676610
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-17 16:52:51 +0000

    UserProfileクラスを作成

---
+++ src/packages/domain/model/userProfile/UserProfile.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace packages\domain\model\userProfile;
+
+class userProfile
+{
+    private UserId $userId;
+    private UserEmail $userEmail;
+    private UserName $userName;
+    private UserPassword $userPassword;
+    private VerificationStatus $verificationStatus;
+
+    private function __construct(
+        UserId $userId,
+        UserEmail $userEmail,
+        UserName $userName,
+        UserPassword $userPassword,
+        VerificationStatus $verificationStatus
+    )
+    {
+        $this->userId = $userId;
+        $this->userEmail = $userEmail;
+        $this->userName = $userName;
+        $this->userPassword = $userPassword;
+        $this->verificationStatus = $verificationStatus;
+    }
+
+    public static function create(
+        UserId $userId,
+        UserEmail $userEmail,
+        UserPassword $userPassword
+    ): self
+    {
+        return new self(
+            $userId,
+            $userEmail,
+            UserName::initialization($userEmail),
+            $userPassword,
+            VerificationStatus::Unverified
+        );
+    }
+
+    public static function reconstruct(
+        UserId $userId,
+        UserEmail $userEmail,
+        UserName $userName,
+        UserPassword $userPassword,
+        VerificationStatus $verificationStatus
+    ): self
+    {
+        return new self(
+            $userId,
+            $userEmail,
+            $userName,
+            $userPassword,
+            $verificationStatus
+        );
+    }
+}
