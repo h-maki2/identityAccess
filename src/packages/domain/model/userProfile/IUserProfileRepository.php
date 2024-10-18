b0bff941b8638c8e34522625e14600ac47354f19
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-18 15:38:05 +0000

    IUserProfileRepositoryインターフェイスを作成

---
+++ src/packages/domain/model/userProfile/IUserProfileRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace packages\domain\model\userProfile;
+
+interface IUserProfileRepository
+{
+    public function findByEmail(UserEmail $email): UserProfile;
+
+    public function findById(UserId $id): UserProfile;
+
+    public function save(UserProfile $userProfile): void;
+
+    public function delete(UserId $id): void;
+
+    public function nextUserId(): UserId;
+}
--- src/packages/test/domain/model/userProfile/UserProfileTest.php
+++ src/packages/test/domain/model/userProfile/UserProfileTest.php
@@ -4,5 +4,8 @@ use PHPUnit\Framework\TestCase;
 
 class UserProfileTest extends TestCase
 {
-    
+    public function test_ユーザープロフィールを初期化する()
+    {
+        
+    }
 }
