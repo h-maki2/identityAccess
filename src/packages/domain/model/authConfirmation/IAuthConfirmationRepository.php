49b1ad8b968b0e8ab86e9984ca633413c9753ce8
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-19 19:10:05 +0000

    IAuthConfirmationRepositoryを作成

--- src/packages/domain/model/authConfirmation/IAuthConfirmationRepository.php
+++ src/packages/domain/model/authConfirmation/IAuthConfirmationRepository.php
@@ -4,5 +4,11 @@ namespace packages\domain\model\authConfirmation;
 
 interface IAuthConfirmationRepository
 {
+    public function findByToken(TemporaryToken $token): AuthConfirmation;
+
+    public function save(AuthConfirmation $authInformation): void;
+
+    public function delete(TemporaryToken $token): void;
+
     public function nextTemporaryToken(): TemporaryToken;
 }
