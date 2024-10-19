c3a5edadfcd0d0e1cc306fad1006e2ef2df94de5
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-19 18:07:15 +0000

    テストケースを追加

--- src/packages/test/service/token/FetchElapsedTimeFromUUIDver7Test.php
+++ src/packages/test/service/token/FetchElapsedTimeFromUUIDver7Test.php
@@ -6,7 +6,7 @@ use Ramsey\Uuid\Uuid;
 
 class FetchElapsedTimeFromUUIDver7Test extends TestCase
 {
-    public function test_UUIDver7が生成されてからの経過時間を正しく取得できる()
+    public function test_UUIDver7が生成されてからの経過時間を正しく取得できる_UUIDver7が生成されてから14時間が経過している場合()
     {
         // given
         $uuidVer7 = Uuid::uuid7();
@@ -23,5 +23,24 @@ class FetchElapsedTimeFromUUIDver7Test extends TestCase
 
         // then
         $this->assertEquals(14, $elapsedTime);
+    }
+
+    public function test_UUIDver7が生成されてからの経過時間を正しく取得できる_UUIDver7が生成されてから25時間が経過している場合()
+    {
+        // given
+        $uuidVer7 = Uuid::uuid7();
+        
+        // 現在から25時間後を取得
+        $dateTime = new DateTime();
+        $interval = new DateInterval('PT25H');
+        $dateTime->add($interval);
+
+        $fetchElapsedTime = new FetchElapsedTimeFromUUIDver7();
+
+        // when
+        $elapsedTime = $fetchElapsedTime->handle($uuidVer7, $dateTime);
+
+        // then
+        $this->assertEquals(25, $elapsedTime);
     }
 }
