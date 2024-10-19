22424b4e870fc44264d9e2a0a30f4a2d7b99cfc9
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-10-19 14:32:24 +0000

    UserProfileTestDataFactory

--- src/packages/test/domain/model/userProfile/helper/UserProfileTestDataFactory.php
+++ src/packages/test/domain/model/userProfile/helper/UserProfileTestDataFactory.php
@@ -2,6 +2,7 @@
 
 namespace packages\test\domain\model\userProfile\helper;
 
+use packages\domain\model\userProfile\IUserProfileRepository;
 use packages\domain\model\userProfile\UserEmail;
 use packages\domain\model\userProfile\UserId;
 use packages\domain\model\userProfile\UserName;
@@ -9,9 +10,16 @@ use packages\domain\model\userProfile\UserPassword;
 use packages\domain\model\userProfile\UserProfile;
 use packages\domain\model\userProfile\VerificationStatus;
 
-class TestUserProfileFactory
+class UserProfileTestDataFactory
 {
-    public static function create(
+    private IUserProfileRepository $userProfileRepository;
+
+    public function __construct(IUserProfileRepository $userProfileRepository)
+    {
+        $this->userProfileRepository = $userProfileRepository;
+    }
+
+    public function create(
         ?UserName $name = null,
         ?UserPassword $password = null,
         VerificationStatus $verificationStatus = VerificationStatus::Verified,
@@ -19,12 +27,16 @@ class TestUserProfileFactory
         UserId $id = new UserId('0188b2a6-bd94-7ccf-9666-1df7e26ac6b8')
     ): UserProfile
     {
-        return UserProfile::reconstruct(
-            $id,
+        $userProfile = TestUserProfileFactory::create(
+            $name,
+            $password,
+            $verificationStatus,
             $email,
-            $name ?? UserName::reconstruct('testUser'),
-            $password ?? UserPassword::reconstruct('ABCabc123_'),
-            $verificationStatus
+            $id
         );
+
+        $this->userProfileRepository->save($userProfile);
+
+        return $userProfile;
     }
 }
