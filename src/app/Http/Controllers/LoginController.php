00d108483efaeb8e47012b9f3f78cf16ebbbbe71
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-04 16:49:47 +0000

    検証のため一時的に作成

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
@@ -23,7 +24,7 @@ class LoginController extends Controller
         // }
         // return;
 
-        $client = Client::where('password_client', 1)->where('id', '4')->first();
+        $client = Client::where('id', '5')->first();
         // return response()->json([
         //     'authorization_url' => url('/oauth/authorize?response_type=code&client_id='.$client->id.'&redirect_uri='.$request->redirect_uri)
         // ]);
@@ -34,6 +35,23 @@ class LoginController extends Controller
 
     public function token(Request $request)
     {
-        var_dump($request['code']);
+        if ($request->method() === 'post') {
+            return;
+        }
+        $response = Http::asForm()->post('http://identity.todoapp.local/oauth/token', [
+            'grant_type' => 'authorization_code',
+            'code' => $request->input('code'),
+            'redirect_uri' => 'http://identity.todoapp.local/test/token',
+            'client_id' => '5',
+            'client_secret' => 'vGbbt7NLl6f7qd2ZSfsFfHOUBgyOtSyyexKOwJc7',
+        ]);
+
+        if ($response->successful()) {
+            print 'aaaaa';
+            $data = $response->json();
+            print_r($data);
+            $accessToken = $data['access_token'];
+            print $accessToken;
+        }
     }
 }
