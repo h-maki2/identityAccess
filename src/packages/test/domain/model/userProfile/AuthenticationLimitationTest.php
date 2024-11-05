0714b19ca99608d56f9b56a30baca124f0633cd0
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-05 16:37:52 +0000

    テストケースを追加

--- src/packages/test/domain/model/userProfile/AuthenticationLimitationTest.php
+++ src/packages/test/domain/model/userProfile/AuthenticationLimitationTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use packages\domain\model\userProfile\AuthenticationLimitation;
+use packages\domain\model\userProfile\FailedLoginCount;
+use packages\domain\model\userProfile\NextLoginAt;
 use PHPUnit\Framework\TestCase;
 
 class AuthenticationLimitationTest extends TestCase
@@ -17,5 +19,76 @@ class AuthenticationLimitationTest extends TestCase
         $this->assertEquals(0, $authenticationLimitation->failedLoginCount());
         // 再ログイン可能な日時はnull
         $this->assertEquals(null, $authenticationLimitation->nextLoginAt());
+    }
+
+    public function test_ログイン失敗回数を更新する()
+    {
+        // given
+        $authenticationLimitation = AuthenticationLimitation::reconstruct(
+            FailedLoginCount::reconstruct(0),
+            null
+        );
+
+        // when
+        $authenticationLimitationAfterChange = $authenticationLimitation->updateFailedLoginCount();
+
+        // then
+        $this->assertEquals(1, $authenticationLimitationAfterChange->failedLoginCount());
+        $this->assertEquals(null, $authenticationLimitationAfterChange->nextLoginAt());
+
+        // 元の値は更新されていないことを確認する
+        $this->assertEquals(0, $authenticationLimitation->failedLoginCount());
+        $this->assertEquals(null, $authenticationLimitation->nextLoginAt());
+    }
+
+    public function test_ログイン失敗回数がアカウントロックのしきい値に達した場合を判定できる()
+    {
+        // given
+        // ログイン失敗回数が10回に達した場合
+        $authenticationLimitation = AuthenticationLimitation::reconstruct(
+            FailedLoginCount::reconstruct(10),
+            null
+        );
+
+        // when
+        $result = $authenticationLimitation->hasReachedAccountLockoutThreshold();
+
+        // then
+        $this->assertTrue($result);
+    }
+
+    public function test_ログイン失敗回数がアカウントロックのしきい値に達していない場合を判定できる()
+    {
+        // given
+        // ログイン失敗回数が10回未満の場合
+        $authenticationLimitation = AuthenticationLimitation::reconstruct(
+            FailedLoginCount::reconstruct(9),
+            null
+        );
+
+        // when
+        $result = $authenticationLimitation->hasReachedAccountLockoutThreshold();
+
+        // then
+        $this->assertFalse($result);
+    }
+
+    public function test_ログイン失敗回数がアカウントロックのしきい値に達した場合、再ログイン可能な日時を設定できる()
+    {
+        // given
+        // ログイン失敗回数が10回に達した場合
+        $authenticationLimitation = AuthenticationLimitation::reconstruct(
+            FailedLoginCount::reconstruct(10),
+            null
+        );
+        $expectedNextLoginAt = NextLoginAt::create();
+
+        // when
+        $authenticationLimitationAfterChange = $authenticationLimitation->updateNextLoginAt();
+
+        // then
+        $this->assertEquals(10, $authenticationLimitation->failedLoginCount());
+        // 再ログイン可能な日時が設定されている
+        $this->assertEquals($expectedNextLoginAt->formattedValue(), $authenticationLimitationAfterChange);
     }
 }
