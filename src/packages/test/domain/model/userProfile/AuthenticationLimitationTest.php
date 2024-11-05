71dd4a4ac7c9c4a5e502c5ee2deb0163dce85d39
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-05 16:17:41 +0000

    テストコードを作成

--- src/packages/test/domain/model/userProfile/AuthenticationLimitationTest.php
+++ src/packages/test/domain/model/userProfile/AuthenticationLimitationTest.php
@@ -14,6 +14,8 @@ class AuthenticationLimitationTest extends TestCase
 
         // then
         // ログイン失敗回数は0回である
-        
+        $this->assertEquals(0, $authenticationLimitation->failedLoginCount());
+        // 再ログイン可能な日時はnull
+        $this->assertEquals(null, $authenticationLimitation->nextLoginAt());
     }
 }
