ceb3a61725b49a54ce85fb20a41deefcb6414abc
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-13 14:01:05 +0000

    テストケースを追加

--- src/packages/test/unit/application/authentication/LoginApplicationServiceTest.php
+++ src/packages/test/unit/application/authentication/LoginApplicationServiceTest.php
@@ -47,7 +47,7 @@ class LoginApplicationServiceTest extends TestCase
         $sessionAuthentication->expects($this->once())
             ->method('markAsLoggedIn')
             ->with($this->callback(function ($userId) use (&$capturedUserId) {
-                $capturedUserId = $userId;
+                $capturedUserId = $userId->value;
                 return true;
             }));
 
@@ -76,6 +76,8 @@ class LoginApplicationServiceTest extends TestCase
         $this->assertTrue($loginResult->loginSucceeded());
         // 認可コード取得用のURLが返されていることを確認する
         $this->assertEquals($clientData->urlForObtainingAuthorizationCode(), $loginResult->authorizationUrl());
+        // 正しいuserIdでログインされていることを確認する
+        $this->assertEquals($userId->value, $capturedUserId);
     }
 
     public function test_メールアドレスが正しくない場合にログインが失敗する()
@@ -176,12 +178,99 @@ class LoginApplicationServiceTest extends TestCase
 
     public function test_アカウントロックの有効期限外の場合、正しいメールアドレスとパスワードでログインできる()
     {
+        // given
+        // 認証情報を作成する
+        $email = new UserEmail('test@example.com');
+        $password = UserPassword::create('ABCabc123_');
+        $userId = $this->authenticationInformaionRepository->nextUserId();
+        // アカウントがロックされているが再ログイン可能
+        $loginRestriction = LoginRestriction::reconstruct(
+            FailedLoginCount::reconstruct(10),
+            LoginRestrictionStatus::Restricted,
+            NextLoginAllowedAt::reconstruct(new DateTimeImmutable('-1 minutes'))
+        );
+        $this->authenticationInformaionTestDataFactory->create(
+            $email,
+            $password,
+            VerificationStatus::Verified,
+            $userId,
+            $loginRestriction
+        );
 
+        // markAsLoggedInメソッドが呼ばれた際に引数の値をキャプチャする
+        $sessionAuthentication = $this->createMock(SessionAuthentication::class);
+        $capturedUserId = null;
+        $sessionAuthentication->expects($this->once())
+            ->method('markAsLoggedIn')
+            ->with($this->callback(function ($userId) use (&$capturedUserId) {
+                $capturedUserId = $userId->value;
+                return true;
+            }));
+
+        // fetchByIdメソッドが呼ばれた際に返すデータを設定する
+        $clientData = TestClientDataFactory::create(
+            null,
+            null,
+            'http://localhost:8080/callback'
+        );
+        $clientFetcher = $this->createMock(IClientFetcher::class);
+        $clientFetcher->method('fetchById')->willReturn($clientData);
+        
+        // when
+        $inputedEmail = 'test@example.com';
+        $inputedPassword = 'ABCabc123_';
+        $clientId = '1';
+        $loginApplicationService = new LoginApplicationService(
+            $this->authenticationInformaionRepository,
+            $sessionAuthentication,
+            $clientFetcher
+        );
+        $loginResult = $loginApplicationService->login($inputedEmail, $inputedPassword, $clientId);
+
+        // then
+        // ログインが成功していることを確認する
+        $this->assertTrue($loginResult->loginSucceeded());
+        // 認可コード取得用のURLが返されていることを確認する
+        $this->assertEquals($clientData->urlForObtainingAuthorizationCode(), $loginResult->authorizationUrl());
+        // 正しいuserIdでログインされていることを確認する
+        $this->assertEquals($userId->value, $capturedUserId);
     }
 
     public function test_ログインに失敗した場合、ログイン失敗回数が更新される()
     {
+        // given
+        // 認証情報を作成する
+        $email = new UserEmail('test@example.com');
+        $password = UserPassword::create('ABCabc123_');
+        $loginRestriction = LoginRestriction::reconstruct(
+            FailedLoginCount::reconstruct(1),
+            LoginRestrictionStatus::Unrestricted,
+            null
+        );
+        $this->authenticationInformaionTestDataFactory->create(
+            $email,
+            $password,
+            VerificationStatus::Verified,
+            null,
+            $loginRestriction
+        );
 
+        // when
+        $inputedEmail = 'test@example.com';
+        // パスワードが間違っている
+        $inputedPassword = 'ABCabc123_!';
+        $clientId = '1';
+        $loginApplicationService = new LoginApplicationService(
+            $this->authenticationInformaionRepository,
+            $this->createMock(SessionAuthentication::class),
+            $this->createMock(IClientFetcher::class)
+        );
+        $loginResult = $loginApplicationService->login($inputedEmail, $inputedPassword, $clientId);
+
+        // then
+        // ログイン失敗回数が更新されていることを確認する
+        $authenticationInformaion = $this->authenticationInformaionRepository->findByEmail($email);
+        $this->assertEquals(2, $authenticationInformaion->loginRestriction()->failedLoginCount());
     }
 
     public function test_ログインに失敗した場合、失敗回数が一定回数を超えた場合アカウントがロックされる()
