b500c3efea45d47170b85b8d124884f2d5aa1687
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-16 14:26:28 +0000

    TemporaryTokenTestを作成

---
+++ src/packages/test/unit/domain/model/authConfirmation/TemporaryTokenTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+use packages\domain\model\authConfirmation\TemporaryToken;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class TemporaryTokenTest extends TestCase
+{
+    public function test_26文字の一時トークンを生成できる()
+    {
+        // given
+
+        // when
+        $temporaryToken = TemporaryToken::create();
+
+        // then
+        $this->assertSame(26, strlen($temporaryToken->value));
+    }
+
+    #[DataProvider('invalidTokenProvider')]
+    public function test_26文字ではない一時トークンを再構築すると例外が発生する($invalidToken)
+    {
+        // given
+
+        // when・then
+        $this->expectException(InvalidArgumentException::class);
+        TemporaryToken::reconstruct($invalidToken);
+    }
+
+    public static function invalidTokenProvider(): array
+    {
+        return [
+            ['c0f1bb19ac3e00100e43efac6'], // 25文字
+            ['c0f1bb19ac3e00100e43efac6e1'], // 27文字
+            ['']
+        ];
+    }
+}
