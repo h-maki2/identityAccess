4651fd192e130be258be9061c895b32bf887690f
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-16 14:19:58 +0000

    TemporaryTokenクラスを作成

---
+++ src/packages/domain/model/authConfirmation/TemporaryToken.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace packages\domain\model\authConfirmation;
+
+use InvalidArgumentException;
+
+class TemporaryToken
+{
+    readonly string $value;
+
+    private const TOKEN_LENGTH = 26;
+
+    private function __construct(string $value)
+    {
+        if (strlen($value) !== self::TOKEN_LENGTH) {
+            throw new InvalidArgumentException('無効なトークンです');
+        }
+
+        $this->value = $value;
+    }
+
+    public static function create(): self
+    {
+        return new self(bin2hex(random_bytes(self::TOKEN_LENGTH / 2)));
+    }
+
+    public static function reconstruct(string $value): self
+    {
+        return new self($value);
+    }
+}
