b3f82add1d32bb216edd595886bfdf3e78aeff48
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-19 15:59:17 +0000

    AuthConfirmationValidationを作成

---
+++ src/packages/application/authentication/verifiedUpdate/display/DisplayVerifiedUpdatePageApplicationService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace packages\application\authentication\verifiedUpdate\display;
+
+use packages\domain\model\authConfirmation\IAuthConfirmationRepository;
+
+/**
+ * 認証済み更新ページを表示するアプリケーションサービス
+ */
+class DisplayVerifiedUpdatePageApplicationService
+{
+    private IAuthConfirmationRepository $authConfirmationRepository;
+
+    public function __construct(IAuthConfirmationRepository $authConfirmationRepository)
+    {
+        $this->authConfirmationRepository = $authConfirmationRepository;
+    }
+
+    /**
+     * 認証済み更新ページを表示する
+     */
+    public function displayVerifiedUpdatePage(string $oneTimeToken)
+    {
+        
+    }
+}
---
+++ src/packages/domain/model/authConfirmation/validation/AuthConfirmationValidation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace packages\domain\model\authConfirmation\validation;
+
+use DateTimeImmutable;
+use packages\domain\model\authConfirmation\AuthConfirmation;
+
+class AuthConfirmationValidation
+{
+    /**
+     * 認証確認が有効かどうかを検証する
+     */
+    public static function validate(?AuthConfirmation $authConfirmation, DateTimeImmutable $currentDateTime): bool
+    {
+        if ($authConfirmation === null) {
+            return false;
+        }
+
+        if ($authConfirmation->isExpired($currentDateTime)) {
+            return false;
+        }
+
+        return true;
+    }
+}
