f2d43779479e11bb17dcd18e5ea38239158456b0
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-19 16:39:03 +0000

    DisplayVerifiedUpdatePageResultを修正

--- src/packages/application/authentication/verifiedUpdate/display/DisplayVerifiedUpdatePageResult.php
+++ src/packages/application/authentication/verifiedUpdate/display/DisplayVerifiedUpdatePageResult.php
@@ -7,25 +7,31 @@ class DisplayVerifiedUpdatePageResult
     readonly bool $validationError;
     readonly string $validationErrorMessage;
     readonly string $oneTimeTokenValue;
+    readonly string $oneTimePassword;
 
     private function __construct(
         bool $validationError,
         string $validationErrorMessage,
-        string $oneTimeTokenValue
+        string $oneTimeTokenValue,
+        string $oneTimePassword
     )
     {
         $this->validationError = $validationError;
         $this->validationErrorMessage = $validationErrorMessage;
         $this->oneTimeTokenValue = $oneTimeTokenValue;
+        $this->oneTimePassword = $oneTimePassword;
     }
 
     public static function createWhenValidationError(string $validationErrorMessage): self
     {
-        return new self(true, $validationErrorMessage, '');
+        return new self(true, $validationErrorMessage, '', '');
     }
 
-    public static function createWhenSuccess(OneTimeTokenValue $oneTimeTokenValue): self
+    public static function createWhenSuccess(
+        OneTimeTokenValue $oneTimeTokenValue,
+        OneTimePassword $oneTimePassword
+    ): self
     {
-        return new self(false, '', $oneTimeTokenValue->value);
+        return new self(false, '', $oneTimeTokenValue->value, $oneTimePassword->value);
     }
 }
---
+++ src/packages/test/unit/application/authentication/verifiedUpdate/display/DisplayVerifiedUpdatePageApplicationServiceTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DisplayVerifiedUpdatePageApplicationServiceTest extends TestCase
+{
+    public function test_
+}
