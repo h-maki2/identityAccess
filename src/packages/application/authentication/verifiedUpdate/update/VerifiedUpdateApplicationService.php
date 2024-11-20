bd3f44e56d710562852d9910c402e2e04b17a3a2
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-20 03:14:12 +0000

    VerifiedUpdateApplicationServiceを作成

--- src/packages/application/authentication/verifiedUpdate/update/VerifiedUpdateApplicationService.php
+++ src/packages/application/authentication/verifiedUpdate/update/VerifiedUpdateApplicationService.php
@@ -8,6 +8,8 @@ use packages\domain\model\authConfirmation\OneTimePassword;
 use packages\domain\model\authConfirmation\OneTimeTokenValue;
 use packages\domain\model\authConfirmation\validation\AuthConfirmationValidation;
 use packages\domain\model\authenticationInformaion\IAuthenticationInformaionRepository;
+use packages\domain\model\common\unitOfWork\UnitOfWork;
+use packages\domain\service\verifiedUpdate\VerifiedUpdate;
 
 /**
  * 認証済み更新を行うアプリケーションサービス
@@ -16,20 +18,26 @@ class VerifiedUpdateApplicationService
 {
     private IAuthenticationInformaionRepository $authenticationInformaionRepository;
     private IAuthConfirmationRepository $authConfirmationRepository;
+    private VerifiedUpdate $verifiedUpdate;
 
     public function __construct(
         IAuthenticationInformaionRepository $authenticationInformaionRepository,
-        IAuthConfirmationRepository $authConfirmationRepository
+        IAuthConfirmationRepository $authConfirmationRepository,
+        UnitOfWork $unitOfWork
     )
     {
-        $this->authenticationInformaionRepository = $authenticationInformaionRepository;
         $this->authConfirmationRepository = $authConfirmationRepository;
+        $this->verifiedUpdate = new VerifiedUpdate(
+            $authenticationInformaionRepository,
+            $this->authConfirmationRepository,
+            $unitOfWork
+        );
     }
 
     /**
      * 認証済み更新を行う
      */
-    public function verifiedUpdate(string $oneTimeTokenValueString, string $oneTimePasswordString)
+    public function verifiedUpdate(string $oneTimeTokenValueString, string $oneTimePasswordString): VerifiedUpdateResult
     {
         $oneTimeTokenValue = OneTimeTokenValue::reconstruct($oneTimeTokenValueString);
         $authConfirmation = $this->authConfirmationRepository->findByToken($oneTimeTokenValue);
@@ -38,6 +46,12 @@ class VerifiedUpdateApplicationService
         }
 
         $oneTimePassword = OneTimePassword::reconstruct($oneTimePasswordString);
-        
+        $verifiedUpdateResult = $this->verifiedUpdate->handle($authConfirmation, $oneTimePassword);
+
+        if (!$verifiedUpdateResult) {
+            return VerifiedUpdateResult::createWhenValidationError('ワンタイムトークンかワンタイムパスワードが無効です。');
+        }
+
+        return VerifiedUpdateResult::createWhenSuccess();
     }
 }
