e4c410e992139869421424251b30015a61597bc9
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-23 16:34:32 +0000

    EloquentAuthenticationInformationRepositoryを作成

--- src/packages/adapter/persistence/eloquent/EloquentAuthenticationInformaionRepository.php
+++ src/packages/adapter/persistence/eloquent/EloquentAuthenticationInformaionRepository.php
@@ -2,7 +2,7 @@
 
 namespace packages\adapter\persistence\eloquent;
 
-use App\Models\AuthenticationInformation;
+use App\Models\AuthenticationInformation as EloquentAuthenticationInformation;
 use DateTimeImmutable;
 use packages\domain\model\common\identifier\IdentifierFromUUIDver7;
 use packages\domain\model\AuthenticationInformation\LoginRestriction;
@@ -21,10 +21,44 @@ class EloquentAuthenticationInformationRepository implements IAuthenticationInfo
 {
     public function findByEmail(UserEmail $email): ?AuthenticationInformation
     {
+        $result = EloquentAuthenticationInformation::where('email', $email->value)->first();
 
+        if ($result === null) {
+            return null;
+        }
+
+        return $this->toAuthenticationInformation($result);
     }
 
-    private function toAuthenticationInformation(AuthenticationInformation $authenticationInformationObj): AuthenticationInformation
+    public function findById(UserId $id): ?AuthenticationInformation
+    {
+        $result = $this->eloquentAuthenticationInformationFrom($id);
+
+        if ($result === null) {
+            return null;
+        }
+
+        return $this->toAuthenticationInformation($result);
+    }
+
+    public function save(AuthenticationInformation $authenticationInformation): void
+    {
+        $eloquentAuthenticationInformation = $this->toEloquentAuthenticationInformation($authenticationInformation);
+        $eloquentAuthenticationInformation->save();
+    }
+
+    public function delete(UserId $id): void
+    {
+        $eloquentAuthenticationInformation = $this->eloquentAuthenticationInformationFrom($id);
+
+        if ($eloquentAuthenticationInformation === null) {
+            // 例外を発生させる予定
+        }
+
+        $eloquentAuthenticationInformation->delete();
+    }
+
+    private function toAuthenticationInformation(EloquentAuthenticationInformation $authenticationInformationObj): AuthenticationInformation
     {
         return AuthenticationInformation::reconstruct(
             new UserId(new IdentifierFromUUIDver7(), $authenticationInformationObj->user_id),
@@ -37,5 +71,29 @@ class EloquentAuthenticationInformationRepository implements IAuthenticationInfo
                 $authenticationInformationObj->next_login_allowed_at !== null ? NextLoginAllowedAt::reconstruct(new DateTimeImmutable($authenticationInformationObj->next_login_allowed_at)) : null
             )
         );
+    }
+
+    private function toEloquentAuthenticationInformation(AuthenticationInformation $authenticationInformation): EloquentAuthenticationInformation
+    {
+        $eloquentAuthenticationInformation = $this->eloquentAuthenticationInformationFrom($authenticationInformation->id());
+
+        if ($eloquentAuthenticationInformation === null) {
+            $eloquentAuthenticationInformation = new EloquentAuthenticationInformation();
+            $eloquentAuthenticationInformation->user_id = $authenticationInformation->id()->value;
+        }
+
+        $eloquentAuthenticationInformation->email = $authenticationInformation->email()->value;
+        $eloquentAuthenticationInformation->password = $authenticationInformation->password()->hashedValue;
+        $eloquentAuthenticationInformation->verification_status = $authenticationInformation->verificationStatus()->value;
+        $eloquentAuthenticationInformation->failed_login_count = $authenticationInformation->loginRestriction()->failedLoginCount();
+        $eloquentAuthenticationInformation->login_restriction_status = $authenticationInformation->loginRestriction()->loginRestrictionStatus();
+        $eloquentAuthenticationInformation->next_login_allowed_at = $authenticationInformation->loginRestriction()->nextLoginAllowedAt();
+
+        return $eloquentAuthenticationInformation;        
+    }
+
+    private function eloquentAuthenticationInformationFrom(UserId $userId): ?EloquentAuthenticationInformation
+    {
+        return EloquentAuthenticationInformation::find($userId->value);
     }
 }
