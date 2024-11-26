14e452e6283ff8410cbab06aa0610830df81f0ea
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-26 14:18:09 +0000

    依存性をプロバイダーに登録

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,14 +2,22 @@
 
 namespace App\Providers;
 
+use EloquentUnitOfWork;
 use Illuminate\Support\ServiceProvider;
+use packages\adapter\oauth\authToken\LaravelPassportAccessTokenDeactivationService;
+use packages\adapter\oauth\authToken\LaravelPassportRefreshokenDeactivationService;
+use packages\adapter\oauth\client\LaravelPassportClientFetcher;
 use packages\adapter\persistence\eloquent\EloquentAuthConfirmationRepository;
 use packages\adapter\persistence\eloquent\EloquentAuthenticationInformationRepository;
 use packages\adapter\presenter\json\authentication\JsonLoginPresenter;
+use packages\adapter\session\LaravelSessionAuthentication;
 use packages\application\authentication\login\LoginOutputBoundary;
 use packages\domain\model\authConfirmation\IAuthConfirmationRepository;
 use packages\domain\model\authenticationInformation\IAuthenticationInformationRepository;
+use packages\domain\model\authenticationInformation\SessionAuthentication;
 use packages\domain\model\common\unitOfWork\UnitOfWork;
+use packages\domain\model\oauth\authToken\IAccessTokenDeactivationService;
+use packages\domain\model\oauth\authToken\IRefreshTokenDeactivationService;
 use packages\domain\model\oauth\client\IClientFetcher;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,14 +31,19 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IAuthConfirmationRepository::class, EloquentAuthConfirmationRepository::class);
         $this->app->bind(IAuthenticationInformationRepository::class, EloquentAuthenticationInformationRepository::class);
 
-        // サービス
-        $this->app->bind(IClientFetcher::class, ::class);
+        // Laravel Passport
+        $this->app->bind(IClientFetcher::class, LaravelPassportClientFetcher::class);
+        $this->app->bind(IAccessTokenDeactivationService::class, LaravelPassportAccessTokenDeactivationService::class);
+        $this->app->bind(IRefreshTokenDeactivationService::class, LaravelPassportRefreshokenDeactivationService::class);
 
         // ユニットオブワーク
-        // $this->app->bind(UnitOfWork::class, EloquentUnitOfWork::class);
+        $this->app->bind(UnitOfWork::class, EloquentUnitOfWork::class);
 
         // プレゼンテーション
         $this->app->bind(LoginOutputBoundary::class, JsonLoginPresenter::class);
+
+        // Laravel
+        $this->app->bind(SessionAuthentication::class, LaravelSessionAuthentication::class);
     }
 
     /**
