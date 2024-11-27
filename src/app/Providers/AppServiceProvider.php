a52681656f4033f511efe68d051bd4ff6bd905d6
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-11-27 14:58:44 +0000

    サービスプロバイダーに登録

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -10,12 +10,28 @@ use packages\adapter\oauth\client\LaravelPassportClientFetcher;
 use packages\adapter\persistence\eloquent\EloquentAuthConfirmationRepository;
 use packages\adapter\persistence\eloquent\EloquentAuthenticationInformationRepository;
 use packages\adapter\presenter\authentication\login\json\JsonLoginPresenter;
+use packages\adapter\presenter\authentication\oneTimeTokenAndPasswordRegeneration\json\JsonOneTimeTokenAndPasswordRegenerationPresenter;
+use packages\adapter\presenter\authentication\userRegistration\json\JsonUserRegistrationPresenter;
+use packages\adapter\presenter\authentication\verifiedUpdate\json\JsonDisplayVerifiedUpdatePagePresenter;
+use packages\adapter\presenter\authentication\verifiedUpdate\json\JsonVerifiedUpdatePresenter;
 use packages\adapter\presenter\errorResponse\ErrorResponse;
 use packages\adapter\presenter\errorResponse\JsonErrorResponse;
 use packages\adapter\session\LaravelSessionAuthentication;
 use packages\application\authentication\login\LoginApplicationService;
 use packages\application\authentication\login\LoginInputBoundary;
 use packages\application\authentication\login\LoginOutputBoundary;
+use packages\application\authentication\oneTimeTokenAndPasswordRegeneration\OneTimeTokenAndPasswordRegenerationApplicationService;
+use packages\application\authentication\oneTimeTokenAndPasswordRegeneration\OneTimeTokenAndPasswordRegenerationInputBoundary;
+use packages\application\authentication\oneTimeTokenAndPasswordRegeneration\OneTimeTokenAndPasswordRegenerationOutputBoundary;
+use packages\application\authentication\verifiedUpdate\display\DisplayVerifiedUpdatePageApplicationService;
+use packages\application\authentication\verifiedUpdate\display\DisplayVerifiedUpdatePageInputBoundary;
+use packages\application\authentication\verifiedUpdate\display\DisplayVerifiedUpdatePageOutputBoundary;
+use packages\application\authentication\verifiedUpdate\update\VerifiedUpdateApplicationService;
+use packages\application\authentication\verifiedUpdate\update\VerifiedUpdateInputBoundary;
+use packages\application\authentication\verifiedUpdate\update\VerifiedUpdateOutputBoundary;
+use packages\application\userRegistration\UserRegistrationApplicationService;
+use packages\application\userRegistration\UserRegistrationInputBoundary;
+use packages\application\userRegistration\UserRegistrationOutputBoundary;
 use packages\domain\model\authConfirmation\IAuthConfirmationRepository;
 use packages\domain\model\authenticationInformation\IAuthenticationInformationRepository;
 use packages\domain\model\authenticationInformation\SessionAuthentication;
@@ -45,14 +61,21 @@ class AppServiceProvider extends ServiceProvider
 
         // プレゼンテーション
         $this->app->bind(LoginOutputBoundary::class, JsonLoginPresenter::class);
+        $this->app->bind(DisplayVerifiedUpdatePageOutputBoundary::class, JsonDisplayVerifiedUpdatePagePresenter::class);
+        $this->app->bind(VerifiedUpdateOutputBoundary::class, JsonVerifiedUpdatePresenter::class);
+        $this->app->bind(OneTimeTokenAndPasswordRegenerationOutputBoundary::class, JsonOneTimeTokenAndPasswordRegenerationPresenter::class);
+        $this->app->bind(UserRegistrationOutputBoundary::class, JsonUserRegistrationPresenter::class);
+        $this->app->bind(ErrorResponse::class, JsonErrorResponse::class);
 
         // Laravel
         $this->app->bind(SessionAuthentication::class, LaravelSessionAuthentication::class);
 
         // アプリケーションサービス
         $this->app->bind(LoginInputBoundary::class, LoginApplicationService::class);
-
-        $this->app->bind(ErrorResponse::class, JsonErrorResponse::class);
+        $this->app->bind(OneTimeTokenAndPasswordRegenerationInputBoundary::class, OneTimeTokenAndPasswordRegenerationApplicationService::class);
+        $this->app->bind(DisplayVerifiedUpdatePageInputBoundary::class, DisplayVerifiedUpdatePageApplicationService::class);
+        $this->app->bind(VerifiedUpdateInputBoundary::class, VerifiedUpdateApplicationService::class);
+        $this->app->bind(UserRegistrationInputBoundary::class, UserRegistrationApplicationService::class);
     }
 
     /**
