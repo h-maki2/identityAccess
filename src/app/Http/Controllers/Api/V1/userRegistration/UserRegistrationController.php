3cca2bbd5007b883fc2ec708565cdc0f023e3455
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-12-11 12:07:55 +0000

    コントローラを修正

--- src/app/Http/Controllers/Api/V1/userRegistration/UserRegistrationController.php
+++ src/app/Http/Controllers/Api/V1/userRegistration/UserRegistrationController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1\userRegistration;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\Request;
+use packages\adapter\presenter\userRegistration\json\JsonUserRegistrationPresenter;
 use packages\application\userRegistration\UserRegistrationInputBoundary;
 
 class UserRegistrationController extends Controller
@@ -23,6 +25,8 @@ class UserRegistrationController extends Controller
             $request->input('passwordConfirmation', '')
         );
 
-        return $output->response();
+        $presenter = new JsonUserRegistrationPresenter($output);
+        $jsonResponseData = $presenter->jsonResponseData();
+        return response()->json($jsonResponseData->value, $jsonResponseData->httpStatusCode());
     }
 }
