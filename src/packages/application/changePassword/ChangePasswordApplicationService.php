ffff7fcb4886c39316d8a44beee8bfc90b4a0f52
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-12-25 14:56:07 +0000

    パスワード変更のアプリケーションサービスを作成

--- src/packages/application/changePassword/ChangePasswordApplicationService.php
+++ src/packages/application/changePassword/ChangePasswordApplicationService.php
@@ -8,6 +8,9 @@ use packages\domain\model\authenticationAccount\UnsubscribeStatus;
 use packages\domain\model\authenticationAccount\UserPassword;
 use packages\domain\model\authenticationAccount\validation\UserPasswordValidation;
 use packages\domain\model\oauth\authToken\AccessToken;
+use packages\domain\model\oauth\client\ClientId;
+use packages\domain\model\oauth\client\IClientFetcher;
+use packages\domain\model\oauth\client\RedirectUrl;
 use packages\domain\model\oauth\scope\IScopeAuthorizationChecker;
 use packages\domain\model\oauth\scope\Scope;
 use packages\domain\model\oauth\scope\ScopeList;
@@ -19,28 +22,39 @@ class ChangePasswordApplicationService
 {
     private IAuthenticationAccountRepository $authenticationAccountRepository;
     private LoggedInUserIdFetcher $loggedInUserIdFetcher;
+    private IClientFetcher $clientFetcher;
 
     public function __construct(
         IAuthenticationAccountRepository $authenticationAccountRepository,
         AuthenticationService $authenticationService,
-        IScopeAuthorizationChecker $scopeAuthorizationChecker
+        IScopeAuthorizationChecker $scopeAuthorizationChecker,
+        IClientFetcher $clientFetcher
     ) {
         $this->authenticationAccountRepository = $authenticationAccountRepository;
         $this->loggedInUserIdFetcher = new LoggedInUserIdFetcher($authenticationService, $scopeAuthorizationChecker);
+        $this->clientFetcher = $clientFetcher;
     }
 
     public function changePassword(
         string $scopeString,
         string $passwordString,
+        string $clientId,
         string $redirectUrl
-    ) 
+    ): ChangePasswordResult
     {
         $scope = Scope::from($scopeString);
         $userId = $this->loggedInUserIdFetcher->fetch($scope);
 
+        if (!$this->isRedirectUrlCorrect(
+            new ClientId($clientId),
+            new RedirectUrl($redirectUrl)
+        )) {
+            throw new RuntimeException('リダイレクトURLが正しくありません');
+        }
+
         $passwordValidation = new UserPasswordValidation($passwordString);
         if (!$passwordValidation->validate()) {
-
+            return ChangePasswordResult::createWhenFaild($passwordValidation->errorMessageList());
         }
 
         $authAccount = $this->authenticationAccountRepository->findById($userId, UnsubscribeStatus::Subscribed);
@@ -51,5 +65,17 @@ class ChangePasswordApplicationService
         $password = UserPassword::create($passwordString);
         $authAccount->changePassword($password, new DateTimeImmutable());
         $this->authenticationAccountRepository->save($authAccount);
+
+        return ChangePasswordResult::createWhenSuccess($redirectUrl);
+    }
+
+    private function isRedirectUrlCorrect(ClientId $clienId, RedirectUrl $redirectUrl): bool
+    {
+        $client = $this->clientFetcher->fetchById($clienId);
+        if ($client === null) {
+            throw new RuntimeException('クライアントが見つかりません');
+        }
+
+        return $client->hasEntereRedirectUrl($redirectUrl);
     }
 }
--- src/packages/application/changePassword/ChangePasswordResult.php
+++ src/packages/application/changePassword/ChangePasswordResult.php
@@ -7,4 +7,25 @@ class ChangePasswordResult
     readonly array $validationErrorMessageList;
     readonly bool $isValidationError;
     readonly string $redirectUrl;
+
+    private function __construct(
+        array $validationErrorMessageList,
+        bool $isValidationError,
+        string $redirectUrl
+    )
+    {
+        $this->validationErrorMessageList = $validationErrorMessageList;
+        $this->isValidationError = $isValidationError;
+        $this->redirectUrl = $redirectUrl;
+    }
+
+    public static function createWhenFaild(array $validationErrorMessageList): self
+    {
+        return new self($validationErrorMessageList, true, '');
+    }
+
+    public static function createWhenSuccess(string $redirectUrl): self
+    {
+        return new self([], false, $redirectUrl);
+    }
 }
