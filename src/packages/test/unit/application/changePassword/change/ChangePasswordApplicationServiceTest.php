f99ebb9844e0d40070db5629b0fdb6af05c7d077
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-12-27 16:44:42 +0000

    テストケースを追加

--- src/packages/test/unit/application/changePassword/change/ChangePasswordApplicationServiceTest.php
+++ src/packages/test/unit/application/changePassword/change/ChangePasswordApplicationServiceTest.php
@@ -3,6 +3,7 @@
 use packages\adapter\persistence\inMemory\InMemoryAuthenticationAccountRepository;
 use packages\application\changePassword\change\ChangePasswordApplicationService;
 use packages\domain\model\authenticationAccount\UnsubscribeStatus;
+use packages\domain\model\authenticationAccount\UserId;
 use packages\domain\model\authenticationAccount\UserPassword;
 use packages\domain\model\oauth\client\ClientId;
 use packages\domain\model\oauth\client\RedirectUrlList;
@@ -140,5 +141,88 @@ class ChangePasswordApplicationServiceTest extends TestCase
         // パスワードが変更されていないことを確認する
         $authAccount = $this->authAccountRepository->findById($authAccount->id(), UnsubscribeStatus::Subscribed);
         $this->assertTrue($authAccount->password()->equals($変更前のパスワード));
+    }
+
+    public function test_パスワード変更後のリダイレクト先URLが正しくない場合に、例外が発生する()
+    {
+        // given
+        // 認証アカウントを作成する
+        $変更前のパスワード = UserPassword::create('acbABC123!');
+        $authAccount = $this->authAccountTestDataCreator->create(password: $変更前のパスワード);
+
+        // ログイン済みのuserIdを取得する処理をモック化する
+        $loggedInUserIdFetcher = $this->createMock(ILoggedInUserIdFetcher::class);
+        $loggedInUserIdFetcher->method('fetch')->willReturn($authAccount->id());
+
+        // oauthのクライアントを作成する
+        $clientId = new ClientId('1');
+        $redirectUrl = 'http://localhost:8080/callback';
+        $redirectUrlList = new RedirectUrlList($redirectUrl);
+        $testClient = $this->testClientDataFactory->create(
+            clientId: $clientId,
+            redirectUriList: $redirectUrlList
+        );
+
+        // テスト用のクライアントデータをフェイクにセットする
+        $this->clientFetcher->setTestClientData($testClient);
+
+        // パスワード変更のアプリケーションサービスを作成する
+        $changePasswordApplicationService = new ChangePasswordApplicationService(
+            $this->authAccountRepository,
+            $this->clientFetcher,
+            $loggedInUserIdFetcher
+        );
+
+        // when・then
+        // リダイレクト先URLが正しくない場合に例外が発生することを確認する
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('リダイレクトURLが正しくありません');
+        $正しくないリダイレクトURL = 'http://localhost:8080/callback2';
+        $changePasswordApplicationService->changePassword(
+            'edit_account',
+            'acbABC1234_',
+            $clientId->value,
+            $正しくないリダイレクトURL
+        );
+    }
+
+    public function test_認証アカウントに紐づいていない不正なuserIdの場合に例外は発生する()
+    {
+        // given
+        $不正なuserId = $this->authAccountRepository->nextUserId();
+
+        // ログイン済みのuserIdを取得する処理をモック化する
+        $loggedInUserIdFetcher = $this->createMock(ILoggedInUserIdFetcher::class);
+        $loggedInUserIdFetcher->method('fetch')->willReturn($不正なuserId);
+
+        // oauthのクライアントを作成する
+        $clientId = new ClientId('1');
+        $redirectUrl = 'http://localhost:8080/callback';
+        $redirectUrlList = new RedirectUrlList($redirectUrl);
+        $testClient = $this->testClientDataFactory->create(
+            clientId: $clientId,
+            redirectUriList: $redirectUrlList
+        );
+
+        // テスト用のクライアントデータをフェイクにセットする
+        $this->clientFetcher->setTestClientData($testClient);
+
+        // パスワード変更のアプリケーションサービスを作成する
+        $changePasswordApplicationService = new ChangePasswordApplicationService(
+            $this->authAccountRepository,
+            $this->clientFetcher,
+            $loggedInUserIdFetcher
+        );
+
+        // when・then
+        // 認証アカウントに紐づいていない不正なuserIdの場合に例外が発生することを確認する
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('ユーザーが見つかりません');
+        $changePasswordApplicationService->changePassword(
+            'edit_account',
+            'acbABC1234_',
+            $clientId->value,
+            $redirectUrl
+        );
     }
 }
